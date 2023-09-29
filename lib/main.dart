d1e65c911221a0ffe92f76dfc83b9d1e4f72139b
Md. Humayun Farid <humayunbee@gmail.com>
2023-09-29 23:24:03 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,123 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CounterScreen(),
+      home: ImageFeedScreen(),
     );
   }
 }
 
-class CounterScreen extends StatefulWidget {
+class ImageFeedScreen extends StatefulWidget {
   @override
-  _CounterScreenState createState() => _CounterScreenState();
+  _ImageFeedScreenState createState() => _ImageFeedScreenState();
 }
 
-class _CounterScreenState extends State<CounterScreen> {
-  int count = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      count++;
-      if (count >= 5) {
-        _showDialog();
-      }
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      if (count > 0) {
-        count--;
-      }
-    });
-  }
-
-  void _showDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Button pressed $count times"),
-          actions: <Widget>[
-            TextButton(
-              child: Text("Close"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+class _ImageFeedScreenState extends State<ImageFeedScreen> {
   @override
   Widget build(BuildContext context) {
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "Counter App",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+        title: Text('News Feed'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Count:",
-              style: TextStyle(fontSize: 24),
-            ),
-            Text(
-              "$count",
-              style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _incrementCounter,
-                  child: Text(
-                    "+",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    minimumSize: Size(50, 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: Text(
-                    "-",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    minimumSize: Size(60, 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: isPortrait ? 1 : 2, 
         ),
+        itemCount: 10, 
+        itemBuilder: (ctx, index) {
+          return Card(
+            elevation: 5,
+            margin: EdgeInsets.all(10),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Image.network(
+                'https://via.placeholder.com/150',
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
